c82b44565a82f20b9337aa63cf37a0ed448c0ae6
kahneraja <ghkpr78>
2014-08-06 23:04:45 +1000

    Additional testing and improvements.

---
+++ ConferenceScheduler.Tests/Session_WhenAllocatingTalks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConferenceScheduler.Common;
+using System.Collections.Generic;
+using ConferenceScheduler.Common.Sessions;
+
+namespace ConferenceScheduler.Tests
+{
+    [TestClass]
+    public class Session_WhenAllocatingTalks
+    {
+        [TestMethod]
+        public void ShouldHaveCorrectStartTime()
+        {
+            var morning = new Morning();
+            var talk = new Talk { Minutes = 1 };
+            morning.Allocate(talk);
+            
+            var startTime = new TimeSpan(9,0,0);
+            Assert.AreEqual(talk.StartTime, startTime);
+        }
+
+        [TestMethod]
+        public void ShouldFillSession()
+        {
+            var afternoon = new Afternoon();
+            afternoon.Allocate(new Talk { Minutes = afternoon.AvailableMinutes });
+
+            var isFull = afternoon.IsFull;
+            Assert.AreEqual(true, isFull);
+        }
+    }
+}
---
+++ ConferenceScheduler.Tests/Store_WhenExportingSchedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConferenceScheduler.Common;
+using System.Collections.Generic;
+using System.Linq;
+using ConferenceScheduler.Common.Store;
+
+namespace ConferenceScheduler.Tests
+{
+    [TestClass]
+    public class Store_WhenExportingSchedule
+    {
+        [TestMethod]
+        public void ShouldLoadTalksAndExportSchedule()
+        {
+            var conferenceSchedule = new ConferenceSchedule();
+
+            var talks = LoadTalks.LoadFile("../../SampleData/Talks.txt");
+
+            conferenceSchedule.AllocateTalks(talks);
+
+            var lines = SaveSchedule.ExportFile(conferenceSchedule, "../../SampleData/EndToEndSchedule.txt");
+
+            Assert.IsNotNull(lines);
+        }
+    }
+}
